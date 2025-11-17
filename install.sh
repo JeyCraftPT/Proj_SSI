d9531cfb8591f51160f87dbef91b8dbf5a330df3
João Pinto <joaoduartelopi@gmail.com>
2025-11-17 10:53:23 +0000

    Activates venv based on detected shell
    Updates the install script to correctly activate the virtual environment based on the user's shell. It now supports fish, bash, and zsh shells.

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Create's a local venv
+# Create a local venv
 python3 -m venv inaben
-# Activate venv (optimize to detect shell)
-if ["echo $SHELL" = "/usr/bin/fish"] ; then 
-	source inaben/bin/activate.fish
-elif [ "echo$SHELL" = "/usr/bin/bash" ]; then
+
+# Detect shell and activate venv
+case "$SHELL" in
+*/fish)
+  source inaben/bin/activate.fish
+  ;;
+*/bash)
   source inaben/bin/activate
-elif [ "echo $SHELL" = "/usr/bin/zsh" ]; then
+  ;;
+*/zsh)
   source inaben/bin/activate
-else
+  ;;
+*)
   echo "Unsupported shell: $SHELL"
-fi
+  exit 1
+  ;;
+esac
 
 # install dependencies used in development of this project
 pip install briefcase
+
